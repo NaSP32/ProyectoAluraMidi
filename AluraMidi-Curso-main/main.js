171124a7a30983ea698d4fdc0450b13142dd4d74
Nadia Soledad Pereyra <nadiapereyra32@gmail.com>
2025-01-05 18:49:03 -0300

    Ajustes evento onkeydown y onkeyup para accesibilidad visual en el codigo

--- AluraMidi-Curso-main/main.js
+++ AluraMidi-Curso-main/main.js
@@ -21,5 +21,22 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
         playSonido(idAudio);
     };
 
+    tecla.onkeydown = function (evento){
+        console.log(evento.code == 'Space')
+        if ( evento.code === 'Space'){
+            
+            tecla.classList.add('activa');
+
+        }
+        
+    }
+
+    tecla.onkeyup = function (){
+        tecla.classList.remove('activa');
+ 
+     }
+    
+   
+
 
 }
