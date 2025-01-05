368f9824ee678b33e48c7a252cb53aec026d6759
Nadia Soledad Pereyra <nadiapereyra32@gmail.com>
2025-01-05 16:50:09 -0300

    Funcion para ejecutar sonido con el evento onclick

---
+++ AluraMidi-Curso-main/main.js
@@ -0,0 +1,17 @@
+/*Creo una funcion para ejecutar un sonido*/
+function playSonidoPom (){
+    document.querySelector('#sonido_tecla_pom').play();
+
+}
+
+document.querySelectorAll('.tecla')
+
+/*Conecto el boton con el evento on clcik para que se ejecute 
+la f. cuando clickeo el boton*/
+//document.querySelector('.tecla_pom').onclick = playSonidoPom;
+
+/* //function playSonidoClap (){
+document.querySelector('#sonido_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = playSonidoClap; */
